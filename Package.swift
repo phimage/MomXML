b7e34b2b04d5a8c9deaa2c3367deecc20811a90d
Eric Marchand <eric.marchand@4d.com>
2019-09-25 15:45:07 +0200

    Update to SWXMLHash 5.0.1

--- Package.swift
+++ Package.swift
@@ -6,19 +6,14 @@ import PackageDescription
 let package = Package(
     name: "MomXML",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MomXML",
             targets: ["MomXML"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "4.9.0")
+        .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "5.0.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MomXML",
             dependencies: [
